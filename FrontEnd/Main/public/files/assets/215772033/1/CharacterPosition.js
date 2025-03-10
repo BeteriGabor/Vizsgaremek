bfb0337f476cf6864f160127ea072a65e62f270f
Bétéri Gábor <beterigabor1@gmail.com>
2025-03-10 08:51:25 +0100

    Update CharacterPosition.js

--- FrontEnd/Main/public/files/assets/215772033/1/CharacterPosition.js
+++ FrontEnd/Main/public/files/assets/215772033/1/CharacterPosition.js
@@ -3,11 +3,11 @@ var CharacterPosition = pc.createScript('characterPosition');
 CharacterPosition.prototype.initialize = function() {
 
     this.targetPositions = [
-        { x: -13, z: 17, url: "http://localhost:3001/sign_in", inside: false },
-        { x: -21, z: 3, url: "http://localhost:3001/slot", inside: false },
-        { x: 19, z: 4, url: "https://www.wikipedia.org", inside: false },
+        { x: -13, z: 17, url: "/sign_in", inside: false },
+        { x: -21, z: 3, url: "/slot", inside: false },
+        { x: 19, z: 4, url: "/", inside: false },
         { x: 23, z: -14, url:"", inside: false},
-        { x: -21, z: -14, url:"http://localhost:3001/blackjack", inside: false},
+        { x: -21, z: -14, url:"/blackjack", inside: false},
     ];
 };
 
