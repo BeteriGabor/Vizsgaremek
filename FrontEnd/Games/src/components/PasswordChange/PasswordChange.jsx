363f942c509a5cc6f299c336263c9bc0eeb58504
Bétéri Gábor <72689243+BeteriGabor@users.noreply.github.com>
2025-03-13 10:15:58 +0100

    Comment add a passwordchange.jsx-ben
    Emlékeztető

--- FrontEnd/Games/src/components/PasswordChange/PasswordChange.jsx
+++ FrontEnd/Games/src/components/PasswordChange/PasswordChange.jsx
@@ -6,6 +6,8 @@ import { Link , useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function PasswordChange(){
+    //előző jelszó lekérése és a név alapján az id megkeresése 
+    
     const [open, setOpen] = useState(true)
     const [showPassword, setShowPassword] = useState(false)
     const handleClickShowPassword = () => setShowPassword((show) => !show);
