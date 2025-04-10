85b96c2f1cf1131f455da3124dd71118d6b29d7f
szebi78 <domi050712@gmail.com>
2025-04-10 11:02:48 +0200

    font fix

--- FrontEnd/Games/tailwind.config.js
+++ FrontEnd/Games/tailwind.config.js
@@ -1,27 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage:{
-        "defbg":"url(./components/assets/bgimage.jpg)",
-        "slotbg":"url(./components/assets/slotbackground.png)",
-        "blackjackbg":"url(./components/assets/blackjack.jpg)",
-        "chickenmap":"url(./components/assets/chickenbackground.png)",
-        "roulette":"url(./components/assets/roulette.png)",
-        "og":"url(./components/assets/othergamesbg.png)",
-        "chickencover":"url(./components/assets/chickencover.png)",
-        "aviatorcover":"url(./components/assets/aviatorcover.png)",
-        "bank":"url(./components/assets/bankbg.png)",
-        "aviatorgamebg":"url(./components/assets/aviatorgamebg.png)",
-        "aviatorbg":"url(./components/assets/aviatorbg.png)",
+      backgroundImage: {
+        defbg: "url(./components/assets/bgimage.jpg)",
+        slotbg: "url(./components/assets/slotbackground.png)",
+        blackjackbg: "url(./components/assets/blackjack.jpg)",
+        chickenmap: "url(./components/assets/chickenbackground.png)",
+        roulette: "url(./components/assets/roulette.png)",
+        og: "url(./components/assets/othergamesbg.png)",
+        chickencover: "url(./components/assets/chickencover.png)",
+        aviatorcover: "url(./components/assets/aviatorcover.png)",
+        bank: "url(./components/assets/bankbg.png)",
+        aviatorgamebg: "url(./components/assets/aviatorgamebg.png)",
+        aviatorbg: "url(./components/assets/aviatorbg.png)",
       },
-      
-      
+      fontFamily: {
+        press: ['"Press Start 2P"', "cursive"], // saját kulcs: font-press
+      },
     },
   },
   plugins: [],
-  
-}
+};
