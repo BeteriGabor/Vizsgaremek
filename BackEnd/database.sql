cdf611f726649080a6bb0e24d2655814245ee2c8
Hansághy Bence <hansaghybeni@gmail.com>
2025-04-28 14:48:45 +0200

    Backend fix

--- BackEnd/database.sql
+++ BackEnd/database.sql
@@ -1,10 +1,1 @@
-CREATE DATABASE `20d_kerekesd_users_management`;
-create table ourusers(
-    id int not null auto_increment,
-    username varchar(255) not null,
-    password varchar(255) not null,
-    birth_Date Date not null,
-    email varchar(255) not null, 
-    role varchar(255) not null,
-    primary key(id)
-);
+CREATE DATABASE `users_management`;
